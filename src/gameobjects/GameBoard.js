fa0ce171595c5ae6dc566bb6f3396d52c3a5937c
miradii <mrtuz@protonmail.com>
2021-10-14 16:28:07 +0330

    added receiving attacks and checking for sinked ships to the gameboard

--- src/gameobjects/GameBoard.js
+++ src/gameobjects/GameBoard.js
@@ -1,28 +1,63 @@
+
 export function createGameBoard() {
   return {
-    grid: Array(10).fill(Array(10)),
+    grid: Array.from(Array(10), () => new Array(10)),
+
     shipsOnTheBoard: new Map(),
+
     numberOfShips: 0,
+
     placeShip(ship, xCordinate, yCordinate) {
+      this.numberOfShips++;
       let startingPoint = 0;
+
       if (ship.isHorizontal) {
         for (let i = 0; i < ship.length; i++) {
           //set location of the grid to the ship id
-          this.grid[xCordinate + i][yCordinate] = this.numberOfShips;
+          this.grid[yCordinate][xCordinate + i] = this.numberOfShips;
         }
         startingPoint = xCordinate;
       } else {
         for (let i = 0; i < ship.length; i++) {
           //set location of the grid to the ship id
-          this.grid[xCordinate][yCordinate + i] = this.numberOfShips;
+          this.grid[yCordinate + i][xCordinate] = this.numberOfShips;
         }
         startingPoint = yCordinate;
       }
+
       let shipCordinates = Array(ship.length).fill(startingPoint);
+
       shipCordinates = shipCordinates.map((el, i) => el + i);
       ship.setCoordinates(shipCordinates);
+
       this.shipsOnTheBoard.set(this.numberOfShips, ship);
-      return ship.getCoordinates();
+
+      return ship;
+    },
+
+    receiveAttack(xCordinate, yCordinate) {
+      const attackedCell = this.grid[yCordinate][xCordinate];
+
+      if (attackedCell > 0) {
+        const attackedShipId = this.grid[yCordinate][xCordinate];
+        const attackedShip = this.shipsOnTheBoard.get(attackedShipId);
+
+        if (attackedShip.isHorizontal) {
+          attackedShip.hit(xCordinate);
+        } else {
+          attackedShip.hit(yCordinate);
+        }
+
+        this.grid[yCordinate][xCordinate] = 0 - attackedShipId;
+      } else if (attackedCell == undefined) {
+        this.grid[yCordinate][xCordinate] = -Infinity;
+      }
+    },
+
+    areAllShipSunk() {
+      let res = true;
+      this.shipsOnTheBoard.forEach((ship) => (res = res && ship.isSunk()));
+      return res;
     },
   };
 }
--- tests/unit/gameboard.spec.js
+++ tests/unit/gameboard.spec.js
@@ -8,5 +8,69 @@ test("we can create a Game Board by calling the factory function", () => {
 test("place ship at the given coordinates and get the ship cordinates", () => {
   const ship = createShip(4);
   const gameBoard = createGameBoard();
-  expect(gameBoard.placeShip(ship, 3, 4)).toStrictEqual([3, 4, 5, 6]);
+  gameBoard.placeShip(ship, 3, 4);
+  expect(ship.getCoordinates()).toStrictEqual([3, 4, 5, 6]);
 });
+
+test("when given an attack the ship located at the position gets hit", () => {
+  const ship = createShip(4);
+
+  const gameBoard = createGameBoard();
+
+  gameBoard.placeShip(ship, 3, 4);
+  gameBoard.receiveAttack(3, 4);
+
+  expect(ship.getCoordinates()).toStrictEqual([-3, 4, 5, 6]);
+
+  gameBoard.receiveAttack(5, 4);
+  expect(ship.getCoordinates()).toStrictEqual([-3, 4, -5, 6]);
+});
+
+test("when some cordinates is a hit the position on the grid is marked by a negative number", () => {
+  const ship = createShip(4);
+  const gameBoard = createGameBoard();
+  gameBoard.placeShip(ship, 3, 4);
+  gameBoard.receiveAttack(3, 4);
+  expect(gameBoard.grid[4][3]).toBeLessThan(0);
+});
+
+test("when an attack misses the position on the board is marked as -Infinity", () => {
+  const gameBoard = createGameBoard();
+  gameBoard.receiveAttack(4, 8);
+  expect(gameBoard.grid[8][4]).toBe(-Infinity);
+});
+
+test("check if all ships are sunk", () => {
+  const ship1 = createShip(2);
+  const ship2 = createShip(3);
+  const ship3 = createShip(2);
+  const ship4 = createShip(4);
+
+  ship4.changeDirection();
+
+  const gameBoard = createGameBoard();
+
+  gameBoard.placeShip(ship1, 2, 3);
+  gameBoard.placeShip(ship2, 4, 7);
+  gameBoard.placeShip(ship3, 3, 2);
+  gameBoard.placeShip(ship4, 7, 2);
+  //sinking ship 1
+  gameBoard.receiveAttack(2, 3);
+  gameBoard.receiveAttack(3, 3);
+  //sinking ship 2
+  gameBoard.receiveAttack(4, 7);
+  gameBoard.receiveAttack(5, 7);
+  gameBoard.receiveAttack(6, 7);
+  //sinking ship 3
+  gameBoard.receiveAttack(3, 2);
+  gameBoard.receiveAttack(4, 2);
+  //sinking ship 4
+  gameBoard.receiveAttack(7, 2);
+  gameBoard.receiveAttack(7, 3);
+  gameBoard.receiveAttack(7, 4);
+  gameBoard.receiveAttack(7, 5);
+
+  expect(gameBoard.areAllShipSunk()).toBe(true);
+});
+
+//TODO make place ship so it throws when you place it on occupied cells
